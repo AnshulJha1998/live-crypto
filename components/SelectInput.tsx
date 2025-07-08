ef017adf2017f37632b81c4c0d1948fc769e3c80
Anshul Jha <anshul.hdr@gmail.com>
2025-07-08 13:35:09 +0530

    setup theme and comps

---
+++ components/SelectInput.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const SelectInput = () => {
+  return (
+    <View>
+      <Text>SelectInput</Text>
+    </View>
+  );
+};
+
+export default SelectInput;
---
+++ utils/common.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  bg: '#191C26',
+  darkBg: '#111319',
+  textCyan: '#4DE0D9',
+  cyanLight: '#8DF3ED',
+  cyanMain: '#34D9D1',
+  pinkLight: '#FF9BD0',
+  pinkMain: '#FF73AA',
+  white: '#FFFFFF',
+  darkGold: '#FDAE67',
+  red: '#FE8270',
+};
