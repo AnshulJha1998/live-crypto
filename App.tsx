e374cfa0c18a71a6a5f02a64efc5ca156cfe3757
Anshul Jha <anshul.hdr@gmail.com>
2025-07-09 17:32:22 +0530

    Added BG Theme on App level

--- App.tsx
+++ App.tsx
@@ -9,21 +9,26 @@ import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { theme } from './utils/common';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import Loading from './screens/loading';
 import Auth from './screens/auth';
 import Location from './screens/location';
 
 const Stack = createNativeStackNavigator();
 
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: theme.bg,
+  },
+};
+
 function App() {
   return (
     <SafeAreaView style={styles.main}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Loading"
-          screenOptions={{ headerShown: false }}
-        >
+      <NavigationContainer theme={MyTheme}>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Loading" component={Loading} />
           <Stack.Screen name="Auth" component={Auth} />
           <Stack.Screen name="Location" component={Location} />
@@ -36,5 +41,4 @@ function App() {
 const styles = StyleSheet.create({
   main: { flex: 1, backgroundColor: theme.bg },
 });
-
 export default App;
